28debb26f8f8ff181f1586bcc07335ab87487451
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-12-13 22:03:13 -0300

    adding business methods and validate logics in order domain

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
@@ -1,13 +1,14 @@
 package com.food.ordering.system.order.service.domain.entity;
 
 import com.food.ordering.system.domain.entity.AggregateRoot;
-import com.food.ordering.system.domain.valueobject.CustomerId;
-import com.food.ordering.system.domain.valueobject.Money;
-import com.food.ordering.system.domain.valueobject.OrderId;
-import com.food.ordering.system.domain.valueobject.RestaurantId;
+import com.food.ordering.system.domain.valueobject.*;
+import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
+import com.food.ordering.system.order.service.domain.valueobject.OrderItemId;
 import com.food.ordering.system.order.service.domain.valueobject.StreetAddress;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 
 import java.util.List;
+import java.util.UUID;
 
 public class Order extends AggregateRoot<OrderId> {
     private final CustomerId customerId;
@@ -18,4 +19,212 @@ public class Order extends AggregateRoot<OrderId> {
     private final Money price;
     private final List<OrderItem> items;
 
+    private TrackingId trackingId;
+    private OrderStatus orderStatus;
+
+    private List<String> failuresMessages;
+
+
+
+    public void initializeOrder(){
+        setId(new OrderId(UUID.randomUUID()));
+        trackingId = new TrackingId(UUID.randomUUID());
+        orderStatus = OrderStatus.PENDING;
+        initializeOrderItems();
+    }
+
+    private void initializeOrderItems() {
+        long itemId = 1;
+        for (OrderItem item: items){
+            item.initializeOrderItem(super.getId(), new OrderItemId(itemId++));
+        }
+    }
+
+    public void validateOrder(){
+        validateInitialOrder();
+        validateTotalPrice();
+        validateItemsPrice();
+    }
+
+    private void validateItemsPrice() {
+        Money orderItemsTotal = items
+                .stream()
+                .map(item -> {
+                    validateItemPrice(item);
+                    return item.getSubTotal();
+                })
+                .reduce(Money.ZERO, Money::add);
+
+        if(!price.equals(orderItemsTotal)){
+            throw new OrderDomainException("Total price: " + price.getAmount() +
+                        "is not equal to Order items total: " + orderItemsTotal.getAmount());
+        }
+    }
+
+    private void validateItemPrice(OrderItem item) {
+        if(!item.isPriceValid()){
+            throw new OrderDomainException("Order item price: " + item.getPrice().getAmount() +
+                    " is not valid for product "+ item.getProduct().getId().getValue());
+        }
+    }
+
+    private void validateTotalPrice() {
+        if(price == null || !price.isGreaterThanZero()){
+            throw new OrderDomainException("Total price must be greater than zero!");
+        }
+    }
+
+    private void validateInitialOrder() {
+        if(orderStatus != null || getId() != null){
+            throw new OrderDomainException("Order is not in correct state for initialization!");
+        }
+    }
+
+    public void pay(){
+        if(orderStatus != OrderStatus.PENDING){
+            throw new OrderDomainException("Order is not in correct state for pay operation");
+        }
+        orderStatus = OrderStatus.PAID;
+    }
+
+    public void approve(){
+        if(orderStatus != OrderStatus.PAID){
+            throw new OrderDomainException("Order is not in correct state for approve operation");
+        }
+        orderStatus = OrderStatus.APPROVED;
+    }
+
+    public void initCancel(List<String> failuresMessages){
+        if(orderStatus != OrderStatus.PAID){
+            throw new OrderDomainException("Order is not in correct state for initCancel operation");
+        }
+        orderStatus = OrderStatus.CANCELLING;
+        updateFailuresMessages(failuresMessages);
+    }
+
+    private void updateFailuresMessages(List<String> failuresMessages) {
+        if(this.failuresMessages != null && failuresMessages != null){
+            this.failuresMessages.addAll(
+                    failuresMessages
+                            .stream()
+                            .filter(message->!message.isEmpty())
+                            .toList()
+            );
+        }
+
+        if(this.failuresMessages == null) {
+            this.failuresMessages = failuresMessages;
+        }
+    }
+
+    public void cancel(){
+        if(!(orderStatus == OrderStatus.CANCELLING || orderStatus == OrderStatus.PENDING)){
+            throw new OrderDomainException("Order is not in correct state for cancel operation");
+        }
+        orderStatus = OrderStatus.CANCELLED;
+    }
+
+    private Order(Builder builder) {
+        super.setId(builder.orderId);
+        customerId = builder.customerId;
+        restaurantId = builder.restaurantId;
+        streetAddress = builder.streetAddress;
+        price = builder.price;
+        items = builder.items;
+        trackingId = builder.trackingId;
+        orderStatus = builder.orderStatus;
+        failuresMessages = builder.failuresMessages;
+    }
+
+    public CustomerId getCustomerId() {
+        return customerId;
+    }
+    public RestaurantId getRestaurantId() {
+        return restaurantId;
+    }
+    public StreetAddress getStreetAddress() {
+        return streetAddress;
+    }
+    public Money getPrice() {
+        return price;
+    }
+    public List<OrderItem> getItems() {
+        return items;
+    }
+    public TrackingId getTrackingId() {
+        return trackingId;
+    }
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+    public List<String> getFailuresMessages() {
+        return failuresMessages;
+    }
+    public static final class Builder {
+        private OrderId orderId;
+        private CustomerId customerId;
+        private RestaurantId restaurantId;
+        private StreetAddress streetAddress;
+        private Money price;
+        private List<OrderItem> items;
+        private TrackingId trackingId;
+        private OrderStatus orderStatus;
+        private List<String> failuresMessages;
+
+        private Builder() {
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder orderId(OrderId val) {
+            orderId = val;
+            return this;
+        }
+
+        public Builder customerId(CustomerId val) {
+            customerId = val;
+            return this;
+        }
+
+        public Builder restaurantId(RestaurantId val) {
+            restaurantId = val;
+            return this;
+        }
+
+        public Builder streetAddress(StreetAddress val) {
+            streetAddress = val;
+            return this;
+        }
+
+        public Builder price(Money val) {
+            price = val;
+            return this;
+        }
+
+        public Builder items(List<OrderItem> val) {
+            items = val;
+            return this;
+        }
+
+        public Builder trackingId(TrackingId val) {
+            trackingId = val;
+            return this;
+        }
+
+        public Builder orderStatus(OrderStatus val) {
+            orderStatus = val;
+            return this;
+        }
+
+        public Builder failuresMessages(List<String> val) {
+            failuresMessages = val;
+            return this;
+        }
+
+        public Order build() {
+            return new Order(this);
+        }
+    }
 }
